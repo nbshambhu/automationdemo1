f0e68be361a67fd9e94dbfe218f1694794a86462
preetijawai72 <preetijawai72@gmail.com>
2025-09-20 19:09:32 +0530

    new project

---
+++ src/test/java/com/automationDemo/automationdemo1/TestCase.java
@@ -0,0 +1,93 @@
+package com.automationDemo.automationdemo1;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestCase {
+	public static WebDriver webDriver;
+
+	@BeforeMethod
+	public WebDriver CreateWebdriver(String browerName) {
+		switch (browerName.toLowerCase()) {
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			webDriver = new ChromeDriver();
+			break;
+		case "firefox":
+		case "mozilla":
+		case "mozilla-firefox":
+			WebDriverManager.firefoxdriver().setup();
+			webDriver = new FirefoxDriver();
+			break;
+		case "edge":
+		case "msedge":
+		case "microsoftedge":
+			WebDriverManager.edgedriver().setup();
+			webDriver = new EdgeDriver();
+			break;
+		case "ie":
+		case "msie":
+		case "microsoftie":
+			WebDriverManager.iedriver().setup();
+			webDriver = new InternetExplorerDriver();
+			break;
+		default:
+			throw new IllegalArgumentException("Unsupported Browser" + browerName);
+		}
+
+		return webDriver;
+	}
+
+	@Test
+	public Boolean launchBrowser(WebDriver webDriver) {
+		try {
+			webDriver.navigate().to("https://procurement.gosmartagro.com/#/login");
+			Duration dr = Duration.ofSeconds(10);
+			WebDriverWait wt = new WebDriverWait(webDriver, dr);
+			wt.until(webdriver -> ((JavascriptExecutor) webdriver).executeScript("return document.readyState", null)
+					.equals("completed"));
+			webDriver.manage().timeouts().implicitlyWait(dr);
+			webDriver.manage().window().fullscreen();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Test
+	public Boolean Login(WebDriver webDriver, String userName, String password) {
+		try {
+			WebElement wl = webDriver.findElement(By.name("userName"));
+			wl.clear();
+			wl.click();
+			wl.sendKeys(userName);
+			wl = webDriver.findElement(By.name("password"));
+			wl.clear();
+			wl.click();
+			wl.sendKeys(password);
+			wl = webDriver.findElement(By.xpath("//button[text()=\"Sign In\"]"));
+			wl.click();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@AfterMethod
+	public void quit() {
+		webDriver.quit();
+	}
+}
